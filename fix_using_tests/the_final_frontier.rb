1a5b6f46f3e6734d7cef83d447287adc8d7c8860
Tom ODay <tomoday91@gmail.com>
2020-09-01 19:47:16 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,9 +1,7 @@
 require 'pry'
 def generate_star_date
-  (rand(100000) + 400000) / 10.0
+ star_date = (rand(100000) + 400000) / 10.0
 end
-
-star_date = generate_star_date
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
